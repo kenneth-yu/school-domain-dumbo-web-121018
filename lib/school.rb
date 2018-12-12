53b447c5c0c4dfc867ecb45d0a20ad21df56a39f
Kenneth Yu <kyu22@buffalo.edu>
2018-12-12 22:08:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+require 'pry'
+class School
+  def initialize (name)
+    @name = name
+    roster = {}
+  end 
+  
+  def roster
+    roster = {}
+  end
+  
+  def add_student (name, grade)
+    roster[grade] = []
+    roster[grade].push(name)
+  end 
+end
